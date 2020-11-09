f883c6f876874dfce6fcea56f258595e3845461e
Tamara <gurcieva.tamara@gmail.com>
2020-11-09 21:14:15 +0300

    Initial commit

---
+++ Assets/GripThrowable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class GripThrowable : Throwable
+{
+    protected override void HandHoverUpdate(Hand hand) {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+
+        if (startingGrabType == GrabTypes.Grip) {
+            hand.AttachObject(gameObject, startingGrabType, attachmentFlags, attachmentOffset);
+            hand.HideGrabHint();
+        }
+    }
+}
